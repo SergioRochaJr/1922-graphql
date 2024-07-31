e5f4d46d751ba888df7da7d2ae3ac83df495d9d7
Sérgio da Rocha <sergiorajr1994@gmail.com>
2024-07-31 14:25:46 -0300

    DataLoader implementado

--- api/turma/datasource/turma.js
+++ api/turma/datasource/turma.js
@@ -1,102 +1,77 @@
-const { SQLDataSource } = require ('datasource-sql')
+const { SQLDataSource } = require('datasource-sql')
 const DataLoader = require('dataloader')
 
 class TurmasAPI extends SQLDataSource {
-    constructor(dbConfig) {
-      super(dbConfig)
-      this.Resposta = {
-        mensagem: ""
-      }
+  constructor(dbConfig) {
+    super(dbConfig)
+    this.Resposta = {
+      mensagem: ""
     }
-  
-    async getTurmas() {
-      return this.db 
-        .select('*')
-        .from('turmas')
-    }
-  
-    async getTurma(id) {
-      const turma = await this.db
-        .select('*')
-        .from('turmas')
-        .where({ id: Number(id)})
-      return turma[0]
-    }
-  
-    async incluiTurma(novaTurma) {
-      const novaTurmaId = await this.db
-        .insert(novaTurma)
-        .returning('id')
-        .into('turmas')
-    
-      const turmaInserida = await this.getTurma(novaTurmaId[0])
-      return ({ ...turmaInserida })
-    }
-  
-    async atualizaTurma(novosDados) {
-      await this.db
-        .update({ ...novosDados.turma })
-        .where({ id: Number(novosDados.id) })
-        .into('turmas')
-  
-      const turmaAtualizada = await this.getTurma(novosDados.id)
-      return ({
-        ...turmaAtualizada
-      })
-    }
-  
-    async deletaTurma(id) {
-      await this.db('turmas')
-        .where({ id: id })
-        .del()
-  
-      this.Resposta.mensagem = "registro deletado"
-      return this.Resposta
-    }
-    
-    getTurmasCarregadas = new DataLoader(async idsTurmas => {
-      const turmas = await this.db
+  }
+
+  async getTurmas({ page = 0, pageOffset = Infinity }) {
+
+    const registroInicial = page === 0 || page === 1 
+      ? 0 
+      : (page * pageOffset) - 1
+
+    return this.db
+      .select('*')
+      .from('turmas')
+      .offset(registroInicial)
+      .limit(pageOffset)
+  }
+
+  async getTurma(id) {
+    const turma = await this.db
+      .select('*')
+      .from('turmas')
+      .where({ id: Number(id)})
+    return turma[0]
+  }
+
+  getTurmasCarregadas = new DataLoader(async idsTurmas => {
+    const turmas = await this.db
       .select('*')
       .from('turmas')
       .whereIn('id', idsTurmas)
 
-      return idsTurmas
+    return idsTurmas
       .map(id => turmas
         .find(turma => turma.id === id))
+  })
+
+  async incluiTurma(novaTurma) {
+    const novaTurmaId = await this.db
+      .insert(novaTurma)
+      .returning('id')
+      .into('turmas')
+  
+    const turmaInserida = await this.getTurma(novaTurmaId[0])
+    return ({ ...turmaInserida })
+  }
+
+  async atualizaTurma(novosDados) {
+    await this.db
+      .update({ ...novosDados.turma })
+      .where({ id: Number(novosDados.id) })
+      .into('turmas')
+
+    const turmaAtualizada = await this.getTurma(novosDados.id)
+    return ({
+      ...turmaAtualizada
     })
   }
 
-//CRIANDO COM APOLLO-DATASOURCE
-/* 
-const DataSource = require('apollo-datasource').DataSource
+  async deletaTurma(id) {
+    await this.db('turmas')
+      .where({ id: id })
+      .del()
 
-const db = [
-    {
-        id:1,
-        descricao: 'básico',
-        horario: 'manhã'
-    },
-    {
-        id: 2,
-        descricao: 'intermediário',
-        horario: 'tarde'
-    }
-]
+    this.Resposta.mensagem = "registro deletado"
+    return this.Resposta
+  }
 
-class TurmasAPI extends DataSource {
-    constructor() {
-        super()
-        this.db = db
-    }
-
-    async getTurmas(){
-        return this.db
-    }
-
-    async getTurma(id){
-        return thisdb.find(turma => turma.id === Number(id))
-    }
-    
 }
-*/
-module.exports = TurmasAPI 
+
+module.exports = TurmasAPI
